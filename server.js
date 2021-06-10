fab4704c913b0241654be68a6d7f34c3137a82b2
omri09 <39883113+omri09@users.noreply.github.com>
2021-06-10 19:44:37 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,13 +6,7 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 app.use(genres);
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/project'));
 
-app.get('/', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/project/index.html'));
-});
 
 
 mongoose.connect('mongodb+srv://omri:987987@cluster0.rrpys.mongodb.net/omri-db?retryWrites=true&w=majority')
