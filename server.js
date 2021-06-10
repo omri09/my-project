e6809a58126c8cf3e37765ddbb1bf0668b85f6e1
omri09 <39883113+omri09@users.noreply.github.com>
2021-06-10 19:41:29 +0300

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(genres);
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/project'));
 
-app.get('/*', function(req,res) {
+app.get('/', function(req,res) {
     
 res.sendFile(path.join(__dirname+'/dist/project/index.html'));
 });
