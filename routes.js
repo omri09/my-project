59fac5895329efc7830bfc9dbfe2788120f4116c
omri <omri620@gmail.com>
2021-06-12 22:52:16 +0300

    jjj

--- routes.js
+++ routes.js
@@ -19,8 +19,7 @@ router.get('/', function (req, res) {
   res.sendFile(__dirname+'/dist/Project/index.html');
 });
 router.get('/orders', function (req, res) {
-  //res.sendFile(__dirname+'/dist/Project/index.html');
-  res.send(path.join(__dirname, '/dist/Project/index.html'));
+  res.sendFile(__dirname+'/dist/Project/index.html');
 
 });
 router.get('/cart', function (req, res) {
